f06466c7b6b1287f3ea4603079291814450ce9ef
Kyle <kyle@goodrx.com>
2017-08-30 14:25:44 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='fwffr',
-    version='0.1.0',
+    version='0.2.0',
     description="Fixed-width field format reader",
     long_description=readme + '\n\n' + history,
     author="Kyle Stark",
