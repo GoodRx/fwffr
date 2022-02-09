d63396db0fc25e9ac5b87d3480ea8cffa9a82bca
Diego Argueta <dargueta@users.noreply.github.com>
2022-02-09 09:48:55 -0800

    Bugfixes

--- HISTORY.rst
+++ HISTORY.rst
@@ -2,7 +2,31 @@
 History
 =======
 
+0.5.0 (2021-02-10)
+------------------
+
+**BREAKING**: Removed ``message`` attribute on exceptions. This was deprecated
+in Python 2.6 and was causing crashes on Python 3.
+
+New
+~~~
+
+* Added official support for Python 3.6 - 3.10.
+* Arguments to exceptions are now preserved as attributes to ease debugging.
+* More documentation.
+* Upgraded development status from "pre-alpha" to "stable".
+
+Bugfixes
+~~~~~~~~
+
+* Fixed `issue 2`_
+* Removed erroneous trove classifier for NLP
+* Added missing ``python_requires`` directive
+* Fixed test suite that wasn't running due to an invalid import
+
 0.1.0 (2017-08-30)
 ------------------
 
 * First release on PyPI.
+
+.. _issue 2: https://github.com/GoodRx/fwffr/issues/2
--- setup.py
+++ setup.py
@@ -38,10 +38,9 @@ setup(
     zip_safe=False,
     keywords='fwffr',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
@@ -49,8 +48,14 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     test_suite='tests',
     tests_require=test_requirements,
     setup_requires=setup_requirements,
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*"
 )
